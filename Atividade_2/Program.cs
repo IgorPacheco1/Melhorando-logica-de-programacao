d06ad0f7eaecc0cd677f0ea33f0c08183b92a1ff
IgorPacheco1 <igopacheco7@gmail.com>
2026-04-20 16:51:45 -0300

    Criando a estrutura de repetição e fazendo o tratamento de dados. (está com erro de logica)

--- Atividade_2/Program.cs
+++ Atividade_2/Program.cs
@@ -9,18 +9,26 @@
 
 //No final, exibir a média e dizer quantos alunos ficaram acima dessa média.
 
-
-
+int ContadorAcimaMedia = 0;
+int Soma = 0;
+double Media=0;
 System.Console.WriteLine("Informe a quantidade de alunos na turma");
 int QuantidadeAlunos = int.Parse(Console.ReadLine());
 
 int[] Alunos = new int[QuantidadeAlunos];
 
-for (int i = 0; i < QuantidadeAlunos.length; i++)
+for (int i = 0; i < Alunos.Length; i++)
 {
-    System.Console.WriteLine($"Informe a nota {i + 1}");
-    QuantidadeAlunos[i] = int.Parse(Console.ReadLine());
+    System.Console.WriteLine($"Informe a nota do {i + 1}° aluno");
+    Alunos[i] = int.Parse(Console.ReadLine());
+    Soma = Soma + Alunos[i];
+    Media = (double) Soma / QuantidadeAlunos;
+    if (Alunos[i]>Media)
+    {
+        ContadorAcimaMedia++;
+    }
 }
+System.Console.WriteLine($"{Soma}");
+System.Console.WriteLine($"{Media.ToString("F2")}");
 
-
-
+System.Console.WriteLine($"O resultado final é o seguinte:\nA media da turma é de {Media}\nQuantidade de alunos que ficaram acima da media foram {ContadorAcimaMedia}");
