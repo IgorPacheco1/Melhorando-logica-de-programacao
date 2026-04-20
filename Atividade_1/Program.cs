2cb7b0fb04c9b8a5cc554910a2b1a497853179da
IgorPacheco1 <igopacheco7@gmail.com>
2026-04-20 14:50:10 -0300

    Adicionando o enunciado e a entrada do valor de N e armazendo em um variavel

--- Atividade_1/Program.cs
+++ Atividade_1/Program.cs
@@ -1,1 +1,11 @@
-﻿// o problema é o seguinte, tenho N provas para serem avaliadas e dentro dessas N notas preciso saber qual a maior nota e quantos alunos ficaram com a nota acima da media 
+﻿//🎯 Nosso Primeiro Desafio de Lógica
+//Imagine que precisamos criar um programa que ajude um professor a analisar as notas de uma turma. O programa deve:
+
+//Pedir a quantidade de alunos (n).
+
+//Ler a nota de cada um desses n alunos.
+
+//No final, mostrar a maior nota e quantos alunos tiraram nota acima de 60.
+
+System.Console.WriteLine("informe a quantidade de alunos que realizaram a prova");
+int n = int.Parse(Console.ReadLine());
