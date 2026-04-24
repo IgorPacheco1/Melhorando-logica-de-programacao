9e29eb722532546578f747fd1c4522c88c19fc5e
IgorPacheco1 <igopacheco7@gmail.com>
2026-04-24 14:44:10 -0300

    Criação da classe para resolução do problema / Criação do vetor para armazenar dados

---
+++ Atividade_5/Program.cs
@@ -0,0 +1,46 @@
+﻿// 📋 Novo Cenário: Sistema de Controle de Estoque (Versão POO)
+// Contexto:
+// Você foi contratado por uma loja de tecnologia para gerenciar o estoque de produtos. Cada produto tem um nome e um preço unitário.
+
+// O Desafio:
+
+// A Classe: Crie uma classe chamada Produto com os atributos: Nome e Preco.
+
+// O Inventário: No Main, pergunte quantos produtos diferentes o usuário deseja cadastrar.
+
+// O Cadastro: Crie um vetor de objetos para armazenar esses produtos e preencha-os via teclado.
+
+// Relatório Final: Após cadastrar tudo, o programa deve exibir:
+
+// A lista de todos os produtos com seus preços.
+
+// O valor total do inventário (a soma de todos os preços).
+
+// O nome do produto mais caro da loja.
+
+
+
+class Produto
+{
+    public string Nome;
+    public double Preco;
+}
+
+class program
+{
+
+
+
+
+
+
+    static void Main(string[] args)
+    {
+        System.Console.WriteLine($"---BEM VINDO A SISTEMA DE CADASTRO DE PRODUTOS--\nQual a quantidade de produtos que deseja cadastrar?");
+        QuantidadeProdutos =  int.Parse(Console.ReadLine());
+
+        Produto[] ListaProdutos = new Produto[QuantidadeProdutos];
+
+        
+    }
+}
