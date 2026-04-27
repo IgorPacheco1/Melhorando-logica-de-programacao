7c48ea1de05d7dcf43b48376ab37c497f950c0d0
Igor Pacheco <igopacheco7@gmail.com>
2026-04-27 20:00:15 -0300

    Criando o vetor e preenchendo ele com numeros aleatorios de 1 a 50

---
+++ Atividade_8/Program.cs
@@ -0,0 +1,19 @@
+﻿class program
+{
+    // static int SomaElementos(int[] V)
+    // {
+
+    // }
+
+    static void Main(string[] args)
+    {
+        int[] VetorNumeros = new int[20];
+        for (int i = 0; i < VetorNumeros.Length; i++)
+        {
+            Random NumAleatorio = new Random();
+            int NumAleatorioAtual = NumAleatorio.Next(1,50);
+            VetorNumeros[i] = NumAleatorioAtual;
+            System.Console.WriteLine(VetorNumeros[i]);
+        }
+    }
+}
